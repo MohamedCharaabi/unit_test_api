3992f43068e77818645da06568c1f5ac0f8d8710
MohamedCharaabi <medch373@gmail.com>
2021-12-12 19:51:14 +0100

    simple Node.js app with test

---
+++ __test__/index.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const app = require("../index");
+
+// describe("Get route", () => {
+//   it("page should return hello world", async (done) => {
+//     const res = await request(app).get("/");
+//     expect(res.statusCode).toEqual(200);
+//     expect(res.body).toEqual("Hello world");
+//     done;
+//   });
+// });
+
+describe("GET /", function () {
+  it("page should return hello world", function (done) {
+    request(app).get("/").expect("Hello world", done);
+  });
+});
+
+// afterAll(async () => {
+//   await app.close();
+// });
--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.status(200).json("Hello world");
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 module.exports = app.listen(process.env.PORT || 4000, () =>
   console.log(`Running on http://localhost:4000`)
 );
